7e8ca26fe8177a6dd5da8ee0c64941601867715c
Alvca <Alvca@DESKTOP-H3LD864>
2020-01-24 09:11:51 +0100

    2

---
+++ src/es/Studium/Ejercicio2.java
@@ -0,0 +1,14 @@
+package es.Studium;
+public class Ejercicio2
+{
+public static boolean esParOImpar(int n) 
+{
+	if(n%2 == 0) {
+		System.out.println("El número es par.");
+		return true;
+	} else {
+		System.out.println("El número es impar.");
+		return false;
+	}
+}
+}
