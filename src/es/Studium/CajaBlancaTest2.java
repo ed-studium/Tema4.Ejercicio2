5a2f8490f8372c686a2d9683d83cd5f09a3c321b
Alvca <Alvca@DESKTOP-H3LD864>
2020-01-27 11:50:39 +0100

    1

---
+++ src/es/Studium/CajaBlancaTest2.java
@@ -0,0 +1,23 @@
+package es.Studium;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CajaBlancaTest2 {
+
+	@Test
+	void testCamino1() {
+		boolean resultadoEsperado = true;
+		boolean resultadoReal= Ejercicio2.esParOImpar(2);
+		assertEquals(resultadoEsperado, resultadoReal);
+	}
+	
+	@Test
+	void testCamino2() {
+		boolean resultadoEsperado = false;
+		boolean resultadoReal= Ejercicio2.esParOImpar(1);
+		assertEquals(resultadoEsperado, resultadoReal);
+	}
+
+}
